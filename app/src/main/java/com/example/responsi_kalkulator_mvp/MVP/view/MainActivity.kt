1412b27f2b35f016a1aebd4208e95c637da29cff
Raihanna1013 <ilfaraihanna10@gmail.com>
2022-04-24 22:44:13 +0700

    first commit

---
+++ app/src/main/java/com/example/responsi_kalkulator_mvp/MVP/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.responsi_kalkulator_mvp.MVP.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.responsi_kalkulator_mvp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
